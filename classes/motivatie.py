9affb6469951c654f658238001da617a486d3203
PADVINDER001 <ibefranckaert7@gmail.com>
2026-03-19 11:28:11 +0100

    class motivatie en oefening

---
+++ classes/motivatie.py
@@ -0,0 +1,6 @@
+class Motivatie:
+    def __init__(self, motivatie):
+        self.motivatie = motivatie
+
+    def __str__(self):
+        return self.motivatie
---
+++ classes/oefening.py
@@ -0,0 +1,6 @@
+class Oefening:
+    def __init__(self, spier):
+        self.spier = spier
+
+    def __str__(self):
+        return f"Spier: {self.spier}"
